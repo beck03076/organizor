242187ff6f6e05ed6720841987ea59e764f771ed
beck03076 <senthilkumar.hce@gmail.com>
2013-10-24 14:25:23 +0530

    Tooltip
    Tooltip

---
+++ app/assets/javascripts/mytooltip.js
@@ -0,0 +1,42 @@
+/*
+ * Tooltip script 
+ * powered by jQuery (http://www.jquery.com)
+ * 
+ * written by Alen Grakalic (http://cssglobe.com)
+ * 
+ * for more info visit http://cssglobe.com/post/1695/easiest-tooltip-and-image-preview-using-jquery
+ *
+ */
+ 
+
+this.tooltip = function(){	
+	/* CONFIG */		
+		xOffset = 10;
+		yOffset = 20;		
+	/* END CONFIG */		
+	$(".tooltip").hover(function(e){											  
+		this.t = this.title;
+		this.title = "";									  
+		$("body").append("<span id='tooltip'>"+ this.t +"</span>");
+		$("#tooltip")
+			.css("top",(e.pageY - xOffset) + "px")
+			.css("left",(e.pageX + yOffset) + "px")
+			.fadeIn("fast");		
+    },
+	function(){
+		this.title = this.t;		
+		$("#tooltip").remove();
+    });	
+	$(".tooltip").mousemove(function(e){
+		$("#tooltip")
+			.css("top",(e.pageY - xOffset) + "px")
+			.css("left",(e.pageX + yOffset) + "px");
+	});			
+};
+
+
+$(document).ready(function(){
+	tooltip();
+
+
+});
