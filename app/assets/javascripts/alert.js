32809e72132a72034be61c7da98df4ba89616c35
beck03076 <senthilkumar.hce@gmail.com>
2013-10-24 16:31:22 +0530

    Popup fix
    Popup fix

--- app/assets/javascripts/alert.js
+++ app/assets/javascripts/alert.js
@@ -1,10 +1,10 @@
 function info(title,content){
     $("#sample-popup").remove();
-    var tag="<div id='sample-popup' class='popuplayout dn'>";
+    var tag="<div id='sample-popup' class='popuplayout addNewPop dn' style='padding:0'>";
     tag+="<div class='popuptitle'><img src='/images/icons/warning_black.png' width=16>&nbsp;"
     tag+= title+"<span class='close-icon bClose fr'>x</span>";
     tag+="<div class='cl'></div></div>";
-    tag+="<div class='popupc'>"+content+"<div><br/>";
+    tag+="<div class='popupc' style='font-size:12px'>"+content+"<div><br/>";
     tag+="<span class='popupok fr bClose' align='center'>OK</span><div class='cl'></div></div></div></div>"
     $("body").append(tag);
     showPopUp('sample-popup');
