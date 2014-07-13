3079f990bb64a1431dbe11031e5f54d5d66c5db6
beck03076 <senthilkumar.hce@gmail.com>
2014-07-13 14:44:17 +0530

    render json to to_json

--- app/controllers/typeaheads_controller.rb
+++ app/controllers/typeaheads_controller.rb
@@ -1,7 +1,8 @@
 class TypeaheadsController < ApplicationController
+	authorize_resource :class => false
 	def results
 	  model = params[:models].singularize.camelize.constantize	
-      results = model.where("#{params[:col]} LIKE '%#{params[:q]}%'")      
-      render json: results
+      results = model.where("#{params[:col]} LIKE '#{params[:q]}%'").to_json      
+      render text: results
     end
 end
