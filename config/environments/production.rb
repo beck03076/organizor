f7cdd419740c026280ab50be80a9e583b1166abe
beck03076 <senthilkumar.hce@gmail.com>
2014-07-31 02:40:14 +0530

    devise produciton

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,4 +68,18 @@ Organizor::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+   #Action Mailer + GMail
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :domain               => 'gmail.com',
+  :user_name            => 'organizor.teamalpha@gmail.com',
+  :password             => 'Organizor@123',
+  :authentication       => 'plain',
+  :enable_starttls_auto => true,
+  :from => 'business@crawlfish.in',
+  :name => "Sprints"  }
 end
