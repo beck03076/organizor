6b142f0b3a83875b42e7791cc82eb8fc663ef5fa
beck03076 <senthilkumar.hce@gmail.com>
2014-07-31 02:46:26 +0530

    devise produciton

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Organizor::Application.configure do
    #Action Mailer + GMail
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
+ # config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
