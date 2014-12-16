3f3ac3556ac7897f84d9f56beb2c7b4146fea048
Mothirajha <rajha12@gmail.com>
2014-12-16 23:45:30 +0530

    On delete of registration remove all approve_request for registration

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -95,7 +95,7 @@ class Registration < ActiveRecord::Base
   has_many :proficiency_exams,class_name: "Exam", dependent: :destroy  
   has_many :documents, dependent: :destroy
   has_many :users, as: :userable
-  has_many :approve_requests
+  has_many :approve_requests, :dependent => :destroy
   #==================
   has_and_belongs_to_many :permissions
   
