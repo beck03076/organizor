7fa100a767435d8ffba6e5fcfeab8c0a0da471b9
beck03076 <senthilkumar.hce@gmail.com>
2015-02-14 14:51:23 +0530

    bugs iteration 1

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -2,7 +2,11 @@ function deactivateGroup(obj,target_div){
   var itemValue = obj.options[obj.selectedIndex].text.toLowerCase();
   if (itemValue == "educational"){  	
   	$(target_div).prop('disabled', false);
+  	$("a.recruitment_territories").show();
+  	$("a.sliding_scales").show();  	
   }else{
   	$(target_div).prop('disabled', true);
+  	$("a.recruitment_territories").hide();
+  	$("a.sliding_scales").hide();
   }
 }
