e4425678b1f54a0a9677593c8cc93e93eeef2aaf
beck03076 <senthilkumar.hce@gmail.com>
2015-03-13 02:43:42 +0530

    remove migrations

--- db/migrate/20150312205132_change_todos_count_to_tasks_count_from_registrations.rb
+++
@@ -1,5 +0,0 @@
-class ChangeTodosCountToTasksCountFromRegistrations < ActiveRecord::Migration
-  def change
-    rename_column :registrations, :todos_count, :tasks_count
-  end
-end
--- db/migrate/20150312205703_change_todos_count_to_tasks_count_from_people.rb
+++
@@ -1,5 +0,0 @@
-class ChangeTodosCountToTasksCountFromPeople < ActiveRecord::Migration
-  def change
-    rename_column :people, :todos_count, :tasks_count
-  end
-end
